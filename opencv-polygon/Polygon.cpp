fa1d2c08f47e28efa0b4415afa368396b4ce09e4
Валерий Радлов <xbcinst@gmail.com>
2017-03-14 08:49:29 +0300

    Место для ветвления для игрищ с альфа-смешиванием.

--- opencv-polygon/Polygon.cpp
+++ opencv-polygon/Polygon.cpp
@@ -11,19 +11,19 @@ using namespace std;
 
 int main() {
 
-//  загружаем исходник
+	//  загружаем исходник
 	Mat src = imread("test0_copy.jpg", 1);
 	namedWindow("src", 0);
 	imshow("src", src);
 
-//  конвертируем в HSV
+	//  конвертируем в HSV
 	Mat hsv;
 	cvtColor(src, hsv, CV_RGB2HSV);
 
-//  разбиваем на каналы
+	//  разбиваем на каналы
 	Mat hsv_channels[3];
 	split(hsv, hsv_channels);
-//  показываем каналы
+	//  показываем каналы
 	namedWindow("H", 0);
 	namedWindow("S", 0);
 	namedWindow("V", 0);
@@ -31,26 +31,26 @@ int main() {
 	imshow("S", hsv_channels[1]);
 	imshow("V", hsv_channels[2]);
 
-//	делаем выборку по насыщенности
+	//	делаем выборку по насыщенности
 	Mat s_range;
 	inRange(hsv_channels[1], 0, 60, s_range);
-//	показываем результат выборки
+	//	показываем результат выборки
 	namedWindow("S_Range", 0);
 	imshow("S_Range", s_range);
 
-//	проходимся ДГК по выборке по насыщенности
+	//	проходимся ДГК по выборке по насыщенности
 	Mat canny;
 	Canny(s_range, canny, 100, 200);
-//	показываем результат работы ДГК
+	//	показываем результат работы ДГК
 	namedWindow("Canny", 0);
 	imshow("Canny", canny);
 
-//	ищем контуры
+	//	ищем контуры
 	vector< vector<Point> > contours;
 	Mat hierarchy;
 	findContours(s_range, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
 
-//	создаём картинку под маску
+	//	создаём картинку под маску
 	Mat mask = Mat::zeros(src.rows, src.cols, CV_8UC1);
 
 	std::ofstream fout;
@@ -76,7 +76,7 @@ int main() {
 					cout << "Point(x,y)=" << contours[i][j] << endl;
 					fout << "Point(x,y)=" << contours[i][j] << endl;
 				}
-				
+
 				cout << endl << "# of contour points: " << contours[i].size() << endl;
 				cout << "Area in px: " << varContourArea << endl;
 				cout << "Area in cm: " << areaInSm << endl;
@@ -91,18 +91,18 @@ int main() {
 	}
 	fout.close();
 
-//	normalize so imwrite(...)/imshow(...) shows the mask correctly!
-//	normalize(mask.clone(), mask, 0.0, 255.0, CV_MINMAX, CV_8UC1);
+	//	normalize so imwrite(...)/imshow(...) shows the mask correctly!
+	//	normalize(mask.clone(), mask, 0.0, 255.0, CV_MINMAX, CV_8UC1);
 
-//	рисуем контуры
-//	CV_FILLED заполняет найденные контуры
+	//	рисуем контуры
+	//	CV_FILLED заполняет найденные контуры
 	drawContours(mask, contours, -1, Scalar(255), CV_FILLED);
 
-//	отображаем нарисованные контуры
+	//	отображаем нарисованные контуры
 	namedWindow("Mask", 0);
 	imshow("Mask", mask);
 
-//	сохраняем необходимые изображения
+	//	сохраняем необходимые изображения
 	imwrite("canny.jpg", canny);
 	imwrite("mask.jpg", mask);
 
